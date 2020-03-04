edadabefdafa82561b58cc28e274d34a9f4437b3
Fatih3287 <kamberi.fatih.87@gmail.com>
2020-03-04 13:59:46 +0100

    avrm node

---
+++ c1/v1.js
@@ -0,0 +1,17 @@
+const tconvert = (t, flag) => {
+    switch (flag) {
+        case "c2f":
+            return (t * 1.8 + 32);
+        case "f2c":
+            return ((t - 32) / 1.8);
+    }
+};
+console.log(tconvert(32, "f2c"));
+console.log(tconvert(100, "c2f"));
+console.log(tconvert(100, "f2c"));
+console.log(tconvert(56, "c2f"));
+console.log(tconvert(56, "abc"));
+
+setInterval(() => {
+    c
+})
